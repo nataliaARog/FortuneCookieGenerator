fa22d99ddfc5d995f1e223ddf4f142c3f9afefbb
root <root@nataia-Inspiron-5537>
2018-10-20 20:16:53 -0300

    Fortune coockie generator fix.

---
+++ FortuneCookieGenerator/src/main/java/com/monsanto/interview/FortuneCookieGenerator/FortuneCookieController.java
@@ -0,0 +1,37 @@
+package com.monsanto.interview.FortuneCookieGenerator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FortuneCookieController {
+
+    @Autowired
+    private QuoteRepository quoteRepository;
+
+    @Autowired
+    private FortuneCookieBuilder fortuneCookieBuilder;
+    
+    /**
+     * Added cleanAll method calling.
+     * It cleans all the values setted before.
+     * It allows to change the values.
+     * @param client
+     * @param company
+     * @return
+     */
+    @GetMapping(path = "/generateFortuneCookie", produces = {"application/json"})
+    public @ResponseBody FortuneCookie generateFortuneCookie(@RequestParam("client") String client,
+                          @RequestParam("company") String company) {
+        String quote = quoteRepository.getRandomQuote();
+        FortuneCookie fortuneCookie = fortuneCookieBuilder.withClient(client).
+                withCompany(company).
+                withQuote(quote).
+                build();
+        return fortuneCookie;
+    }
+
+}
